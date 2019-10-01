de96864eaef8e2b204573eef9a14517e6b2821f5
brunovongxay <vongxay.bruno@gmail.com>
2019-10-01 12:04:57 +0200

    Ajout de l'exercice 7 avec commentaires des différents programme.

--- exo_07_b.rb
+++ exo_07_b.rb
@@ -1,3 +1,6 @@
+# Ce programme invite l'utilisateur à répondre à une question 
+# et lui indique l'endroit où saisir sa réponse grace au chevron dans le print.
+
 puts "Bonjour, c'est quoi ton blase ?"
 print "> "
 user_name = gets.chomp
--- exo_07_c.rb
+++ exo_07_c.rb
@@ -1,2 +1,4 @@
+# Ce programme insère un champs de saisi mais sans donner d'indication à l'utilisateur.
+
 user_name = gets.chomp
 puts user_name
