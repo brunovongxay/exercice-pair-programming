564bee34d7f358c9e8137fa620c3c0fed5d8c6c8
brunovongxay <vongxay.bruno@gmail.com>
2019-10-01 17:28:14 +0200

    Ajout exo 18

---
+++ exo_18.rb
@@ -0,0 +1,15 @@
+list = []
+k = 0
+
+50.times do 
+    if k < 10
+        list << "jean.dupont.0#{k}@email.fr"
+        puts "jean.dupont.0#{k}@email.fr"
+    else 
+        list << "jean.dupont.0#{k}@email.fr"
+        puts "jean.dupont.#{k}@email.fr"
+    end
+    k+=1
+    
+end
+
