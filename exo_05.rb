b9e47596a66e9db660a6440bc4fbd488e077a377
brunovongxay <vongxay.bruno@gmail.com>
2019-10-01 11:18:25 +0200

    Ajout exo 5 avec commentaires

---
+++ exo_05.rb
@@ -0,0 +1,38 @@
+# cette ligne affiche un string
+puts "On va compter le nombre d'heures de travail à THP"
+
+# cette ligne affiche un string et les #{} permettent le calcule des valeurs à l'intérieur du string
+puts "Travail : #{10 * 5 * 11}"
+
+# cette ligne affiche un string et les #{} permettent le calcule des valeurs à l'intérieur du string
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+# cette ligne affiche un string
+puts "Et en secondes ?"
+
+# cette ligne affiche le résultat de l'opération
+puts 10 * 5 * 11 * 60 * 60
+
+# cette ligne affiche la chaine de caractères ainsi que l'opération (sans afficher le résultat)
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+# cette ligne affiche un booléen
+puts 3 + 2 < 5 - 7
+
+# cette ligne affiche un string, une opération et le résultat de l'opération 3 + 2 avec #{}
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+# cette ligne affiche un string
+puts "Ok, c'est faux alors !"
+
+# cette ligne affiche un string
+puts "C'est drôle ça, faisons-en plus :"
+
+# cette ligne affiche un string ainsi qu'un booléen
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+
+
+ 
