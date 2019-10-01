0f7d5c72c0cfcf02b47d7337288dcc4ee1738a47
brunovongxay <vongxay.bruno@gmail.com>
2019-10-01 17:29:26 +0200

    Ajout exo 18

--- exo_18.rb
+++ exo_18.rb
@@ -6,7 +6,7 @@ k = 0
         list << "jean.dupont.0#{k}@email.fr"
         puts "jean.dupont.0#{k}@email.fr"
     else 
-        list << "jean.dupont.0#{k}@email.fr"
+        list << "jean.dupont.#{k}@email.fr"
         puts "jean.dupont.#{k}@email.fr"
     end
     k+=1
